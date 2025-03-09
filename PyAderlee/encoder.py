fe57170c3a6cedf797f1d83ab5813be6646b1e4f
Khaled Karman <k@rawasy.com>
2025-03-09 03:20:10 -0400

    Uncomment and activate JSON and CSV conversion methods in Encoder
    - Restore previously commented-out static methods for JSON and CSV conversion
    - Enable to_json, from_json, and to_csv methods for data serialization
    - Maintain existing method signatures and functionality

--- PyAderlee/encoder.py
+++ PyAderlee/encoder.py
@@ -146,27 +146,27 @@ class Encoder:
 
     
     # """
-    # @staticmethod
-    # def to_json(data: Any, indent: int = 4) -> str:
-    #     """Convert data to JSON string"""
-    #     return json.dumps(data, indent=indent)
+    @staticmethod
+    def to_json(data: Any, indent: int = 4) -> str:
+        """Convert data to JSON string"""
+        return json.dumps(data, indent=indent)
     
-    # @staticmethod
-    # def from_json(json_str: str) -> Any:
-    #     """Parse JSON string to Python object"""
-    #     return json.loads(json_str)
+    @staticmethod
+    def from_json(json_str: str) -> Any:
+        """Parse JSON string to Python object"""
+        return json.loads(json_str)
     
-    # @staticmethod
-    # def to_csv(data: List[Dict], delimiter: str = ',') -> str:
-    #     """Convert list of dictionaries to CSV string"""
-    #     if not data:
-    #         return ""
+    @staticmethod
+    def to_csv(data: List[Dict], delimiter: str = ',') -> str:
+        """Convert list of dictionaries to CSV string"""
+        if not data:
+            return ""
         
-    #     headers = list(data[0].keys())
-    #     rows = [delimiter.join(headers)]
+        headers = list(data[0].keys())
+        rows = [delimiter.join(headers)]
         
-    #     for item in data:
-    #         row = [str(item.get(header, '')) for header in headers]
-    #         rows.append(delimiter.join(row))
+        for item in data:
+            row = [str(item.get(header, '')) for header in headers]
+            rows.append(delimiter.join(row))
             
-    #     return '\n'.join(rows) 
+        return '\n'.join(rows) 
